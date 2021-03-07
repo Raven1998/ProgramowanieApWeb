e64bf006c0ccbd446e3f9632abe5f602fb828427
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2021-03-07 13:03:59 +0100

    Start class created

---
+++ Lab1/src/index.ts
@@ -0,0 +1,22 @@
+class Start{
+    //Inputy liczbowe
+    input1 :HTMLInputElement;
+    input2 :HTMLInputElement;
+    input3 :HTMLInputElement;
+    input4 :HTMLInputElement;
+
+    //Inputy wynikow
+    suminput:HTMLInputElement;
+    avginput:HTMLInputElement;
+    mininput:HTMLInputElement;
+    maxinput:HTMLInputElement;
+
+    //Konstruktor
+    constructor(){
+        this.run;
+    }
+
+    run(){
+        
+    }
+}
