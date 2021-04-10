262324f6c9eceeebf23c5d63981ca4a251082c55
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2021-04-10 21:44:51 +0200

    Lab 1 minimal version ts script created

--- Lab1/src/index.ts
+++ Lab1/src/index.ts
@@ -1,4 +1,4 @@
-class Start{
+class StatsApp{
     //Inputy liczbowe
     input1 :HTMLInputElement;
     input2 :HTMLInputElement;
@@ -6,17 +6,62 @@ class Start{
     input4 :HTMLInputElement;
 
     //Inputy wynikow
-    suminput:HTMLInputElement;
-    avginput:HTMLInputElement;
-    mininput:HTMLInputElement;
-    maxinput:HTMLInputElement;
+    sumInput:HTMLInputElement;
+    avgInput:HTMLInputElement;
+    minInput:HTMLInputElement;
+    maxInput:HTMLInputElement;
 
     //Konstruktor
     constructor(){
-        this.run;
+        this.startApp;
     }
 
-    run(){
-        
+    startApp(){
+        this.getInputs();
+        this.watchInputValues();
+    }
+
+    //Query selectory do inputów
+    getInputs(){
+
+        this.input1 =document.querySelector('#numberone');
+        this.input2 =document.querySelector('#numbertwo');
+        this.input3 =document.querySelector('#numberthree');
+        this.input4 =document.querySelector('#numberfour');
+        this.sumInput =document.querySelector('#sum');
+        this.avgInput =document.querySelector('#avg');
+        this.minInput =document.querySelector('#min');
+        this.maxInput =document.querySelector('#max');
+    }
+
+    watchInputValues() {
+        this.input1.addEventListener('input', () => this.computeData());
+        this.input2.addEventListener('input', () => this.computeData());
+        this.input3.addEventListener('input', () => this.computeData());
+        this.input4.addEventListener('input', () => this.computeData());
+    }
+
+    computeData(){
+        const data1 = +this.input1.value;
+        const data2 = +this.input2.value;
+        const data3 = +this.input3.value;
+        const data4 = +this.input4.value;
+
+        const sum=data1+data2+data3+data4;
+        const avg = sum/4;
+
+        const min =Math.min(data1,data2,data3,data4);
+        const max =Math.max(data1,data2,data3,data4);
+        this.showStats(sum, avg, min, max);
+
+    }
+
+    showStats(sum:number,avg:number,min:number,max:number){
+        this.sumInput.value =sum.toString();
+        this.avgInput.value =avg.toString();
+        this.minInput.value =min.toString();
+        this.maxInput.value =max.toString();
     }
 }
+
+const statsApp =new StatsApp();
