5e17b28e8c6fa1dd2ddceb6e373fdc1b06428a48
Rafał Kruczkowski <rk.rafalkruczkowski@gmail.com>
2021-04-11 12:45:26 +0200

    Lab 3 Update

--- Lab1/src/index.ts
+++ Lab1/src/index.ts
@@ -1,9 +1,11 @@
 class StatsApp{
     
-    dataInput1 :HTMLInputElement;
-    dataInput2 :HTMLInputElement;
-    dataInput3 :HTMLInputElement;
-    dataInput4 :HTMLInputElement;
+    //dataInput1 :HTMLInputElement;
+    //dataInput2 :HTMLInputElement;
+    //dataInput3 :HTMLInputElement;
+    //dataInput4 :HTMLInputElement;
+    quantityInput:HTMLInputElement;
+    
 
     sumInput:HTMLInputElement;
     avgInput:HTMLInputElement;
@@ -20,13 +22,22 @@ class StatsApp{
         this.watchInputValues();
     }
 
+    createInputs(){
+        this.quantityInput =document.querySelector('#amount');
+        const amount =parseInt(this.quantityInput.value, 10);
+
+        for()
+
+
+
+    }
     //Query selectory do inputów
     getInputs(){
-
-        this.dataInput1 =document.querySelector('#data1');
-        this.dataInput2 =document.querySelector('#data2');
-        this.dataInput3 =document.querySelector('#data3');
-        this.dataInput4 =document.querySelector('#data4');
+        
+        //this.dataInput1 =document.querySelector('#data1');
+        //this.dataInput2 =document.querySelector('#data2');
+        //this.dataInput3 =document.querySelector('#data3');
+        //this.dataInput4 =document.querySelector('#data4');
         this.sumInput =document.querySelector('#sum');
         this.avgInput =document.querySelector('#avg');
         this.minInput =document.querySelector('#min');
@@ -34,24 +45,24 @@ class StatsApp{
     }
 
     watchInputValues() {
-        this.dataInput1.addEventListener('input', () => this.computeData());
-        this.dataInput2.addEventListener('input', () => this.computeData());
-        this.dataInput3.addEventListener('input', () => this.computeData());
-        this.dataInput4.addEventListener('input', () => this.computeData());
+       // this.dataInput1.addEventListener('input', () => this.computeData());
+        //this.dataInput2.addEventListener('input', () => this.computeData());
+       // this.dataInput3.addEventListener('input', () => this.computeData());
+        //this.dataInput4.addEventListener('input', () => this.computeData());
     }
 
     computeData(){
-        const data1 = +this.dataInput1.value;
-        const data2 = +this.dataInput2.value;
-        const data3 = +this.dataInput3.value;
-        const data4 = +this.dataInput4.value;
+        //const data1 = +this.dataInput1.value;
+        //const data2 = +this.dataInput2.value;
+        //const data3 = +this.dataInput3.value;
+        //const data4 = +this.dataInput4.value;
 
-        const sum=data1+data2+data3+data4;
-        const avg = sum/4;
+        //const sum=data1+data2+data3+data4;
+       // const avg = sum/4;
 
-        const min =Math.min(data1,data2,data3,data4);
-        const max =Math.max(data1,data2,data3,data4);
-        this.showStats(sum, avg, min, max);
+        //const min =Math.min(data1,data2,data3,data4);
+        //const max =Math.max(data1,data2,data3,data4);
+       // this.showStats(sum, avg, min, max);
 
     }
 
